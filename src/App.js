3678bca4b76fed965e73ff7c710015ff269d1a78
Jerry Chang <jerry@danceclout.com>
2021-01-28 17:51:46 +0800

    save 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
 	return (
 		<div className="app">
-			<h1>Lets build the facebook clone!</h1>
+			<Header />
+			{/* App body */}
+			{/* Sidebar */}
+			{/* Feed */}
+			{/* Widgets */}
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBjByUNYDNLqMVwXsvuRnNJZKxMGGwK-QI",
+	authDomain: "facebook-clone-5052d.firebaseapp.com",
+	projectId: "facebook-clone-5052d",
+	storageBucket: "facebook-clone-5052d.appspot.com",
+	messagingSenderId: "455942408592",
+	appId: "1:455942408592:web:6e1f90aa965d155e544b15",
+	measurementId: "G-PXTX4HX2VX",
+};
